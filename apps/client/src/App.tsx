bbebb17053dac2a63566e6e494e660958b880e2c
Maikel <maikelberbi@gmail.com>
2024-04-12 11:02:27 -0400

    Testing changes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/Login.page'
-// import TestingPage from './pages/Test.page'
 import SignUpPage from './pages/SignUp.page'
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/signup' element={<SignUpPage/>}/>
-        {/* <Route path='/testing' element={<TestingPage/>}/> */}
 
       </Routes>
    </Router>
--- apps/client/src/pages/Test.page.tsx
+++ apps/client/src/pages/Test.page.tsx
@@ -1,15 +1,1 @@
-import { createContext, useContext, useState } from "react";
 
-type Theme = 'light' | 'dark' | 'system';
-const themeContext = createContext<Theme>('system');
-
-const useGetTheme = () => useContext(themeContext)
-
-export function TestingPage() {
-  const [theme, setTheme] = useState<Theme>('system');
-  
-  return(
-    <
-  )
-
-}
