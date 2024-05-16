5db9a36dcfdf6fcb1aa7078d6610a8c11c8f8cd5
Maikel <maikelberbi@gmail.com>
2024-05-15 21:33:52 -0400

    react router dom added

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/Login.page'
 import SignUpPage from './pages/SignUp.page'
 import { Profile } from './pages/Profile'
+import HomePage from './pages/Home.page'
+
 
 
 export default function App() {
   return (
    <Router>
       <Routes>
-        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/' element={<HomePage/>}/>
         <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
         <Route path='/profile' element={<Profile/>}/>
 
       </Routes>
