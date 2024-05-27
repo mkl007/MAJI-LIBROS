08fa35a07bef7c877e3633e5837ef45a0d28d85e
Maikel <maikelberbi@gmail.com>
2024-05-27 02:37:26 -0400

    saving changes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,20 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/Login.page'
 import SignUpPage from './pages/SignUp.page'
 import { Profile } from './pages/Profile'
-import HomePage from './pages/Home.page'
+import {HomePage} from './pages/Home.page'
+import { NavBar } from './components/NavBar.component'
+import { Products } from './pages/Products.page'
 
 
 
 export default function App() {
   return (
-   <Router>
+    <Router>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/signup' element={<SignUpPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/products' element={<Products />} />
 
       </Routes>
-   </Router>
+    </Router>
   )
 }
--- apps/client/src/components/NavBar.component.tsx
+++ apps/client/src/components/NavBar.component.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+
+
 
 export const NavBar = () => {
   return (
-    <div>NavBar.component</div>
+    <>
+      <div className=' bg-[1da1f2]'>
+        <div>
+          <div>
+            <nav>
+              <Link to='/'><img src="" alt="Maji books" /></Link>
+            </nav>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li><Link to='/products'>Products</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
