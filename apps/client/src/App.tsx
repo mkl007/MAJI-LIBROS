af7897542337745882f29d358a5520e867ed0f9a
Maikel <maikelberbi@gmail.com>
2024-05-29 19:14:14 -0400

    adding navbar

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,21 +3,23 @@ import LoginPage from './pages/Login.page'
 import SignUpPage from './pages/SignUp.page'
 import { Profile } from './pages/Profile'
 import {HomePage} from './pages/Home.page'
-import { NavBar } from './components/NavBar.component'
+import { Navbar } from './components/NavBar.component'
 import { Products } from './pages/Products.page'
+import { Settings } from './pages/Settings.page'
 
 
 
 export default function App() {
   return (
     <Router>
-      <NavBar />
+      <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/products' element={<Products />} />
+        <Route path='/settings' element={<Settings />} />
 
       </Routes>
     </Router>
