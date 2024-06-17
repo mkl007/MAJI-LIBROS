e2f12aeb3168507132954015bd632ef122b714b9
Maikel <maikelberbi@gmail.com>
2024-06-17 16:36:00 -0400

    Sign up componet performed.

--- apps/client/src/components/GithubButton.component.tsx
+++ apps/client/src/components/GithubButton.component.tsx
@@ -1,21 +1,36 @@
-import instanceAxios from "../api/axiosSetup";
+// import instanceAxios from "../api/axiosSetup";
+
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 export const GithubButton = () => {
-    
+
     const handleLogin = () => {
         // window.location.href = 'http://localhost:3000/auth/github';
         window.location.href = `http://localhost:3000/api/v1/auth/github/`
         // window.location.href = `${instanceAxios}/auth/github/callback`
-        
+
     };
     return (
         <div>
-            <button onClick={handleLogin}
-            className="flex border-3 border-red-500"
-            >
-                Login with GitHub
-            </button>
+
+            <div className="flex items-center justify-between my-1">
+                <span className="border-b border-gray-300 w-full"></span>
+                <span className="px-4 text-gray-500">or</span>
+                <span className="border-b border-gray-300 w-full"></span>
+            </div>
+            <div className="flex flex-col space-y-4">
+                <button className="flex  items-center w-full mt-3 justify-center rounded-md px-3 py-2 text-sm font-semibold leading-6  shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-red-600 text-white  hover:bg-red-700 transition duration-200">
+                    <FaGoogle className="mr-2" />
+                    Google
+                </button>
+                <button
+                    onClick={handleLogin}
+                    className="flex items-center justify-center w-full bg-gray-800 text-white py-2 rounded-md hover:bg-gray-900 transition duration-200">
+                    <FaGithub className="mr-2" />
+                    GitHub
+                </button>
+            </div>
         </div>
     );
 }
--- apps/client/src/pages/SignUp.page.tsx
+++ apps/client/src/pages/SignUp.page.tsx
@@ -1,134 +1,14 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { UserToSignUp } from '../interfaces/User.interface';
-// import { signUpRequest } from '../api/auth';
-import { useAuth } from '../hooks/useAuth';
-import { GithubButton } from '../components/GithubButton.component';
-
-
-
-const FormLoginComponent = () => {
-  const [user, setUser] = useState<UserToSignUp>({ fullname: '', email: '', password: '' });
-
-  const { data, signUpFunction } = useAuth()
-
-  async function handleSubmit(event: { preventDefault: () => void; }) {
-    event.preventDefault();
-    await signUpFunction(user);
-
-  }
-
-  console.log(data)
-  return (
-    // <div className="container bg-midnight bg-stone-300">
-    <div className=" container mb-3.5 bg-midnight bg-stone-50 mt-10 sm:mx-auto sm:w-full sm:max-w-sm rounded-md border-0 border-neutral-800 shadow-2xl">
-
-      {/* <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 container border-3 border-fuchsia-800"> */}
-      {/* <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className=""
-            src="../../public/logo-no-bg.png"
-            alt="Maji Book"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          </h2>
-        //// this is section is commited because the form is not needed in this component.
-        </div>  */}
-
-
-      {/* <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm border-1 border-neutral-800 shadow-2xl"> */}
-      <div className="container">
-        <div><h1>Signup</h1></div>
-        <form onSubmit={handleSubmit} className="space-y-6" >
-          <div>
-
-            <div className="mt-2">
-              <input
-                id="fullname"
-                name="fullname"
-                type="fullname"
-                autoComplete="fullname"
-                required
-                placeholder='Fullname'
-                onChange={(e) => setUser({ ...user, fullname: e.target.value })}
-                className="block w-full rounded-md border-2 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder-gray-500 focus:border-sky-900 sm:text-sm sm:leading-6"
-
-              // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus: border-sky-400  sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                placeholder='Email'
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                className="block w-full rounded-md border-2 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder-gray-500 focus:border-sky-900 sm:text-sm sm:leading-6"
-
-              // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                placeholder='Password'
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                className="block w-full rounded-md border-2 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder-gray-500 focus:border-sky-900 sm:text-sm sm:leading-6"
-
-              // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-
-            <div
-              className="flex w-full justify-center rounded-md bg-zinc-950 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-zinc-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              <GithubButton />
-            </div>
-
-          </div>
-        </form>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Already have an accoun?{' '}
-          <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Log in
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
-};
+import { FormLoginComponent } from "../components/SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <div className='w-full h-screen bg-midnight bg-cyan-700 border-3 border-fuchsia-800'>
-      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-        <h1 className='text-xl text-cyan-50'>MAJIBOOKS</h1>
+    <div className='w-full h-screen bg-midnight bg-slate-100'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm rounded-md'>
+        <h1 className='text-xl text-cyan-900'>MAJIBOOKS</h1>
       </div>
       <FormLoginComponent />
     </div>
+
   )
 };
 
