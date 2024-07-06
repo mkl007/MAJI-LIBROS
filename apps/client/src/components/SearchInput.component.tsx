236be52139ff41509494d22203a75cfe9b9dbca4
Maikel <maikelberbi@gmail.com>
2024-07-06 12:26:21 -0400

    NavBar component performed

--- apps/client/src/components/SearchInput.component.tsx
+++ apps/client/src/components/SearchInput.component.tsx
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 export const SearchInput = () => {
     return (
 
-            <div className="flex items-center w-80 border-1 border-gray-600 rounded-lg bg-gray-800 py-1.5 px-3 shadow-sm sm:leading-6">
+            <div className="flex items-center w-100% border-1 border-gray-600 rounded-lg bg-gray-800 py-1.5 px-3 shadow-sm sm:leading-6">
                 <input
                     id="search"
                     name="search"
@@ -15,10 +15,10 @@ export const SearchInput = () => {
                     className="block w-full bg-gray-800  text-gray-900 placeholder-gray-50 focus:outline-none focus:text-gray-100"
                 />
                 <div className="bb">
-                    <button type="submit">
+                    {/* <button type="submit"> */}
                         <FaSearch className="mr-2 text-gray-50" />
 
-                    </button>
+                    {/* </button> */}
                 </div>
 
         </div>
