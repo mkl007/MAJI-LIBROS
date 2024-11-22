2ac5c51cef44049fe9d905d5856a1f3c41bb8308
mkl007 <maikelberbi@gmail.com>
2024-11-22 09:13:26 -0400

    cloudinary config added and file uploaded on the backen

--- apps/server/routes/books.route.js
+++ apps/server/routes/books.route.js
@@ -2,8 +2,13 @@ import express from 'express'
 import { myBooks, newBook, singleBook } from '../controllers/books.controller.js';
 export const routerBook = express.Router();
 import fileupload from 'express-fileupload'
+import { uploadedFilefunction } from '../utils/cloudinary.js';
 //
-routerBook.use(fileupload())
+// routerBook.use(fileupload())
+routerBook.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: './uploadTemp'
+}));
 //
 
 routerBook.post('/newbook/:userId', newBook)
@@ -12,11 +17,16 @@ routerBook.get('/:userId/books', myBooks);
 
 routerBook.get('/book/:userId/:singlebook', singleBook)
 //
-routerBook.post('/upload', (req, res) => {
+routerBook.post('/upload', async (req, res) => {
     if (!req.files || Object.keys(req.files) === 0) return res.status(400).json({ message: 'No files were uploaded.' });
 
-    const uploadedFile = req.files.foo;
-    console.log('hay foo', uploadedFile)
-    return res.status(200).json({uploadedFile})
+    try {
+        const uploadedFile = req.files.foo;
+        const result = await uploadedFilefunction(uploadedFile.tempFilePath)
+        console.log(result)
+        return res.status(200).json({ uploadedFile })
+    } catch (error) {
+        console.log(error)
+    }
 })
 //
