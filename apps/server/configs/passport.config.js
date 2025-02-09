0b2fb658a297453b1b4a3044af7773f8f784c06f
mkl007 <maikelberbi@gmail.com>
2025-02-08 23:45:11 -0400

    final url replacement

--- apps/server/configs/passport.config.js
+++ apps/server/configs/passport.config.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GitHubStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: `${DEPLOYED_API_URI}/auth/github/callback`,
+  callbackURL: `${process.env.DEPLOYED_API_URI}/auth/github/callback`,
   // callbackURL: 'http://localhost:3000/api/v1/auth/github/callback',
   scope: ['email']
 }, async (accessToken, refreshToken, profile, done) => {
