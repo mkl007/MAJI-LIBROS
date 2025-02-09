7b4cadbcbc146d0275495f2d3f3713a31cc46b4a
mkl007 <maikelberbi@gmail.com>
2025-02-08 23:41:50 -0400

    replacing github uri

--- apps/server/configs/passport.config.js
+++ apps/server/configs/passport.config.js
@@ -1,15 +1,14 @@
 import passport from 'passport';
 import { Strategy as GitHubStrategy } from 'passport-github2';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import User from '../models/user.model.js';
-import Token from '../models/token.model.js';
+
 
 dotenv.config();
 passport.use(new GitHubStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/api/v1/auth/github/callback',
+  callbackURL: `${DEPLOYED_API_URI}/auth/github/callback`,
+  // callbackURL: 'http://localhost:3000/api/v1/auth/github/callback',
   scope: ['email']
 }, async (accessToken, refreshToken, profile, done) => {
   done(null, profile);
