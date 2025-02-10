cc2b6df7aa9c9f5c91c8cb4db40d089dd9f301e2
mkl007 <maikelberbi@gmail.com>
2025-02-10 18:28:38 -0400

    production url set

--- apps/client/src/components/NavBar.component.tsx
+++ apps/client/src/components/NavBar.component.tsx
@@ -138,7 +138,7 @@ export const Navbar = () => {
                                                         {({ active }) => (
                                                             <Link
                                                                 // onClick={() => 'http://localhost:3000/api/v1/logout'}
-                                                                onClick={() => window.location.href = `${import.meta.env.VITE_API_URL}/lo`}
+                                                                onClick={() => window.location.href = `${import.meta.env.VITE_API_URL}/logout`}
                                                                 to="#"
                                                                 className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                             >
