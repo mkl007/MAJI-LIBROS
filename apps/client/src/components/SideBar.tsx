1e88b13d8d5c3c4b64053ea8e2ab57020ca996ca
mkl007 <maikelberbi@gmail.com>
2025-07-07 00:13:25 -0400

    sidebar styled

--- apps/client/src/components/SideBar.tsx
+++ apps/client/src/components/SideBar.tsx
@@ -8,27 +8,30 @@ export const SideBar = () => {
       <nav>
         <h1 className=" md:block hidden text-2xl font-bold pb-6">Settings</h1>
 
-        <div className="flex md:hidden gap-4 overflow-x-auto whitespace-nowrap px-4 py-2 ">
+        <div className="flex md:hidden gap-4 overflow-x-auto whitespace-nowrap px-4 py-2 
+                           ">
           {menuRoutes.map((routeTo) => (
+
             <Link key={routeTo.title} to={routeTo.to} about={routeTo.description}
-            className="border-b-2 border-red-500 hover:bg-gray-400 "
+              className=" hover:bg-gray-200 hover:rounded-2xl flex-1"
             >
               <i className="text-xl">{routeTo.icon}</i>
             </Link>
+
           ))}
         </div>
 
         <div className="hidden md:block">
           {menuRoutes.map((routeTo) => (
             <div className="hover:bg-gray-300" key={routeTo.title}>
-            
+
               <Link
                 className=" flex items-center space-x-2 text-slate-950 hover:text-blue-950 transition duration-300 text-sm"
                 to={routeTo.to}
                 about={routeTo.description}
-              > 
-                <i className="text-xs text-center">{routeTo.icon} </i> 
-                
+              >
+                <i className="text-xs text-center">{routeTo.icon} </i>
+
                 <span>{routeTo.title}</span>
               </Link>
             </div>
