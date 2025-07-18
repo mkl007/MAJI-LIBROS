145d47930a9877b84b61ab77ab117819081a805c
mkl007 <maikelberbi@gmail.com>
2025-07-18 10:38:03 -0400

    search ui fixed

--- apps/client/src/components/ui/SearchInput.tsx
+++ apps/client/src/components/ui/SearchInput.tsx
@@ -13,7 +13,6 @@ export const SearchInput = () => {
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
-        console.log("Search text changed:", e.target.value);
     };
 
     useEffect(() => {
@@ -54,14 +53,9 @@ export const SearchInput = () => {
             </button>
 
             {showInput && (
-                <div
-                 className="fixed top-0 left-0 w-full h-16 sm:h-16  z-10 bg-white shadow-md
-                                 p4  border-2 border-green-500 lg:hidden"
-
-                >
-                    <div
-                        className="w-full max-w-2xl flex "
-                    >
+                <div className="fixed top-0 left-0 w-full h-16 sm:h-16  z-10 bg-white shadow-md
+                                  flex ">
+                    <div className="w-full max-w-2xl flex items-center justify-between px-4 ">
                         <button
                             onClick={toggleInput}
                             className="text-gray-600 hover:text-red-600 p-2 rounded-lg mt-2  "
@@ -78,12 +72,18 @@ export const SearchInput = () => {
                             required
                             type="text"
                             disabled={false}
-                            className="w-9/12 px-4 py-2 rounded-lg focus:outline-none focus:ring-0
-                                        "
-                   
+                            className="w-full px-4 py-2 mr-64 rounded-lg focus:outline-none focus:ring-0
+                                         "
                         />
 
                     </div>
+                    <button
+                        onClick={() => console.log("Search")}
+                        className="text-gray-600 hover:text-red-600 p-4  rounded-lg "
+                        title=" search"
+                    >
+                        <FaSearch />
+                    </button>
                 </div>
             )}
         </div>
